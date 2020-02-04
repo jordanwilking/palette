740457eaa17a6a54346507c51a5076e7085789c5
jwilk <jordanwilking@yahoo.com>
2020-02-03 18:34:30 -0700

    Add palette list delete transition group and hidden localStorage reset button

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import MiniPalette from './MiniPalette'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import MiniPalette from './MiniPalette'
 import mediaSizes from './mediaSizes'
 import bg from './bg.svg'
+import { Button } from '@material-ui/core'
 
 const Page = styled.div`
   display: flex;
@@ -55,13 +57,33 @@ const CreateLink = styled(Link)`
   color: rgba(255, 255, 255, 0.5);
 `
 
-const PalettesContainer = styled.div`
+const PalettesContainer = styled(TransitionGroup)`
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
   align-content: flex-start;
   height: 100%;
   width: 100%;
+
+  .fade-exit-active {
+    opacity: 0;
+    transition: opacity 500ms ease-out;
+  }
+`
+
+const Reset = styled(Button)`
+  && {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    z-index: 10;
+    opacity: 0;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
 `
 
 // TODO: can't do justify-content flex-start and space-between at the same time
@@ -75,29 +97,41 @@ class PaletteList extends Component {
     this.props.history.push(`palette/${id}`)
   }
 
+  handleResetClick() {
+    window.localStorage.clear()
+    window.location.reload()
+  }
+
   render() {
     const { deletePalette, palettes } = this.props
 
     return (
-      <Page>
-        <PalettesPageContainer>
-          <PalettesHeader>
-            <AppName>React Colors</AppName>
-            <CreateLink to='/palette/new'>Create Palette</CreateLink>
-          </PalettesHeader>
-          <PalettesContainer>
-            {palettes.map(palette => (
-              <MiniPalette
-                key={palette.id}
-                id={palette.id}
-                palette={palette}
-                onClick={this.goToPalette}
-                onDelete={deletePalette}
-              />
-            ))}
-          </PalettesContainer>
-        </PalettesPageContainer>
-      </Page>
+      <>
+        <Page>
+          <PalettesPageContainer>
+            <PalettesHeader>
+              <AppName>React Colors</AppName>
+              <CreateLink to='/palette/new'>Create Palette</CreateLink>
+            </PalettesHeader>
+            <PalettesContainer>
+              {palettes.map(palette => (
+                <CSSTransition key={palette.id} classNames='fade' timeout={500}>
+                  <MiniPalette
+                    key={palette.id}
+                    id={palette.id}
+                    palette={palette}
+                    onClick={this.goToPalette}
+                    onDelete={deletePalette}
+                  />
+                </CSSTransition>
+              ))}
+            </PalettesContainer>
+          </PalettesPageContainer>
+        </Page>
+        <Reset onClick={this.handleResetClick} variant='contained'>
+          Reset Local Storage
+        </Reset>
+      </>
     )
   }
 }
